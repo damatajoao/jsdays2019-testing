9d04ade750f1c3d31317461e90d232119c4ada0e
Joao da Mata <damata@mediaworx.de>
2019-10-08 13:51:49 +0200

    clock und tick

---
+++ spec/clock.spec.js
@@ -0,0 +1,15 @@
+fdescribe('async', () => {
+    beforeEach(() => {
+        jasmine.clock().install();
+    });
+    afterEach(() => {
+        jasmine.clock().uninstall();
+    });
+    it('should be done after 5000ms', (done) => {
+       executesAsyncWithClock((v) => {
+           expect(v).toBe('Hello World');
+           done();
+       });
+       jasmine.clock().tick(5000);
+    });
+});
